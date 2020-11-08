0df1ff18ac49393245044f18a18c5451be1f1274
danguinamrata <danguinamrata@gmail.com>
2020-11-07 21:01:13 -0500

    Basic UI + Basic Mulitpart Upload

--- src/main/java/com/dangui/namrata/controller/BasicController.java
+++ src/main/java/com/dangui/namrata/controller/BasicController.java
@@ -1,38 +1,69 @@
 package com.dangui.namrata.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
 
 import com.dangui.namrata.model.*;
+import com.dangui.namrata.utils.*;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
-@RestController
+@Controller
 public class BasicController {
     private final Logger log = LoggerFactory.getLogger(this.getClass());
     @Autowired
     private CustomerRepository repository;
 
-    @RequestMapping("/")
+    @Autowired
+    FileService fileService;
+
+    @GetMapping("/")
     public String index() {
-        return "Greetings from Spring Boot";
+        return "index";
 
     }
 
-    @RequestMapping("/customer")
-    public List<Customer> displayCustomer(){
+    @GetMapping("/customer")
+    public String displayCustomer(Model model){
         log.info("Showing Mongo Customer records .........................................");
         List<Customer> result = new ArrayList<Customer>();
-        for(Customer customer: repository.findByLastName("Dangui") ) {
+        for(Customer customer: repository.findByLastName("Dangui1") ) {
             result.add(customer);
         }
-        //return String.join(" ", result);
-        return result;
+        model.addAttribute("customer", result);
+        return "listCustomer";
+    }
+
+    @GetMapping("/uploadFile")
+    public String uploadGet() {
+        log.info("I am here finally ..............");
+        return "upload";
+    }
+
+    @PostMapping(value = "/uploadFile")
+    public String uploadFile(@RequestParam("file")MultipartFile file, RedirectAttributes redirectAttributes){
+        try {
+            fileService.uploadFile(file);
+
+            redirectAttributes.addFlashAttribute("message",
+                    "You have successfully uploaded " + file.getOriginalFilename() + "!"
+            );
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+
+        }
+
+        return "redirect:/";
     }
 }
---
+++ src/main/java/com/dangui/namrata/utils/FileService.java
@@ -0,0 +1,27 @@
+package com.dangui.namrata.utils;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.util.StringUtils;
+
+import java.io.File;
+import java.nio.file.*;
+
+@Service
+public class FileService {
+
+    @Value("${app.upload.dir:${user.home}}")
+    public String uploadDir;
+
+    public void uploadFile(MultipartFile file) {
+        try {
+            Path copyLocation = Paths.get(uploadDir + File.separator + StringUtils.cleanPath(file.getOriginalFilename()));
+            Files.copy(file.getInputStream(), copyLocation, StandardCopyOption.REPLACE_EXISTING);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new FileStorageException("Error uploading File" + file.getOriginalFilename()
+                    + ". Please try again!");
+        }
+    }
+}
